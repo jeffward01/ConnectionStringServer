7053e5cba590bf8d6ae5a745ea7fab155eb01ec2
jeffward01-umpg <jeff.ward@umusic.com>
2017-04-09 03:11:23 -0700

    added goHome to add project

--- ConStrServer.Web/ConStrServer.Web/app/Controllers/addProjectCtrl.js
+++ ConStrServer.Web/ConStrServer.Web/app/Controllers/addProjectCtrl.js
@@ -4,7 +4,9 @@ app.controller('addProjectCtrl',
         '$scope', 'stateManager',
         function ($scope, stateManager) {
 
-
+            $scope.goHome = function() {
+                stateManager.goHome();
+            }
 
         }
     ]);
