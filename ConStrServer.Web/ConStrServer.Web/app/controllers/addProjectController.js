7177ff4bbcbe997a2237d93941dbd8eb945b4a4e
jeffward01-umpg <jeff.ward@umusic.com>
2017-08-03 00:12:08 -0700

    working on API

--- ConStrServer.Web/ConStrServer.Web/app/controllers/addProjectController.js
+++ ConStrServer.Web/ConStrServer.Web/app/controllers/addProjectController.js
@@ -1,23 +1,22 @@
 ﻿angular.module("app").controller('addProjectController',
     [
-        '$scope', '$state', 'authService', 'stateManager', 'alertService', 'smoothScroll', '$rootScope', '$timeout', 'objectService', 'projectService', 
+        '$scope', '$state', 'authService', 'stateManager', 'alertService', 'smoothScroll', '$rootScope', '$timeout', 'objectService', 'projectService',
         function ($scope, $state, authService, stateManager, alertService, smoothScroll, $rootScope, $timeout, objectService, projectService) {
-
-
+            $scope.envrironmentsToAdd = [];
             $scope.project = {}
+            $scope.newEnvironment = {};
 
             $scope.submitProject = function () {
                 if ($scope.project.ProjectName == null || $scope.project.ProjectName.length == 0) {
                     alertService.error("Please add a project name");
                 } else {
-                    
+                    assembleProject();
                     projectService.createProject($scope.project).then(function (result) {
-                        clearProject();
                         alertService.success("Project has been added!");
                         $state.go("app.viewProject",
-                        {
-                            projectId: result.ProjectId
-                    });
+                            {
+                                projectId: result.ProjectId
+                            });
                     },
                         function (err) {
                             console.log(JSON.stringify(err));
@@ -26,16 +25,22 @@
                 }
             }
 
-            $scope.addTech = function (tech) {
-                if ($scope.newTech.technologyName == null) {
+            $scope.removeEnvironment = function (env) {
+                objectService.removeElementFromArray(env, $scope.envrironmentsToAdd);
+            }
+
+            $scope.addEnvironment = function (tech) {
+                if ($scope.newEnvironment.EnvironmentName == null || $scope.newEnvironment.EnvironmentName.length === 0) {
                     return;
-                } else if ($scope.newTech.technologyName.length === 0) {
-                    return;
-                } else {
-                    $scope.technologiesToAdd.push($scope.newTech);
-                    $scope.newTech = null;
+                }
+                else {
+                    $scope.envrironmentsToAdd.push($scope.newEnvironment);
+                    $scope.newEnvironment = null;
                 }
             }
 
+            function assembleProject() {
+                $scope.project.Environments = $scope.envrironmentsToAdd;
+            }
         }
     ]);
